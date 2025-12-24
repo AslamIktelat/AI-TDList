20fe8bf33267dce94928a46c6fc761fc4491eea7
AslamIktelat <59434331+AslamIktelat@users.noreply.github.com>
2025-12-24 16:45:15 +0200

    Adding Spring security to dependencies

--- src/main/java/com/ai/tdlist/repository/TaskRepository.java
+++ src/main/java/com/ai/tdlist/repository/TaskRepository.java
@@ -2,7 +2,10 @@ package com.ai.tdlist.repository;
 
 import com.ai.tdlist.entities.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
+@Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
 
     public TaskEntity save(TaskEntity taskEntity);
