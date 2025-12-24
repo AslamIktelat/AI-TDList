36c60e8f4e83cdecb608fd232e18415bfdeeb57d
AslamIktelat <59434331+AslamIktelat@users.noreply.github.com>
2025-12-24 17:08:09 +0200

    Add security controller for CSRF token retrieval and configure default user credentials

---
+++ src/main/java/com/ai/tdlist/controllers/SecurityControllers.java
@@ -0,0 +1,18 @@
+package com.ai.tdlist.controllers;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/security")
+public class SecurityControllers {
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+     return (CsrfToken) request.getAttribute("_csrf");
+
+    }
+}
